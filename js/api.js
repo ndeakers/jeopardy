6a539f8c82c9c54977ea92525ef0fc581f6340fd
Nate Deakers <ndeakers@gmail.com>
2021-08-18 21:15:00 -0700

    updated to fetch data over https

--- js/api.js
+++ js/api.js
@@ -1,5 +1,5 @@
 "use strict";
-const BASE_API_URL = "http://jservice.io/api";
+const BASE_API_URL = "https://jservice.io/api";
 class Api {
   static async get(endpoint, parameters) {
     let response = await axios.get(`${BASE_API_URL}/${endpoint}`, { params: parameters });
